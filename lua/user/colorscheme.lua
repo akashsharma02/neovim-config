a49f8b8228efa8f7e5e882443206f229c523e879
Akash Sharma <akashsharma@cmu.edu>
2022-02-17 10:16:16 -0500

    Remove catpuccin

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,18 +1,3 @@
-local catppuccin = require("catppuccin")
-
--- configure it
-catppuccin.setup({
-    transparent_background = true,
-    integrations = {
-        nvimtree = {
-            enabled = true,
-            show_root = false,
-            transparent_panel = true
-        },
-        which_key = true,
-    }
-})
-
 local kanagawa = require("kanagawa")
 kanagawa.setup({
     undercurl = true,
