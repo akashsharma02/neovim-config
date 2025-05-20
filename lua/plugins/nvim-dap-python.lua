18e00400e410c1e0c521e92bd933dab840c06890
Akash Sharma <akashsharma@cmu.edu>
2025-05-20 11:51:47 -0400

    Update

--- lua/plugins/nvim-dap-python.lua
+++ lua/plugins/nvim-dap-python.lua
@@ -1,6 +1,6 @@
 return {
-  "mfussenegger/nvim-dap-python",
-  config = function()
-    require("dap-python").setup("~/mambaforge/envs/debugpy/bin/python")
-  end,
+    "mfussenegger/nvim-dap-python",
+    config = function()
+        require("dap-python").setup("~/miniforge3/envs/debugpy/bin/python")
+    end,
 }
