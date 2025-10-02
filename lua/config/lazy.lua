57c91229079b52cc32d5e4ab5fff2693fb7694df
Akash Sharma <akashsharma@cmu.edu>
2025-10-02 17:24:39 -0400

    Update lazyvim config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,11 +11,10 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.ui.alpha" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-
-vim.g.python3_host_prog = "/home/akashsharma/mambaforge/bin/python3"
+vim.g.python3_host_prog = "/home/akashsharma/miniforge3/bin/python3"
 
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
